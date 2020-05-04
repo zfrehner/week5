883f297883430ff6fbd38d5a80c338dc50b6ea60
Zach Frehner <zfrehner@mail.greenriver.edu>
2020-05-04 17:34:00 -0400

    F3 practice and using the Hive

--- index.php
+++ index.php
@@ -10,8 +10,28 @@ require_once("vendor/autoload.php");
 $f3 = Base::instance();
 
 //define a default root (what the user sees when they go to index page)
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
     //echo "<h1>Hello</h1>";
+
+    //create some variables in the F3 HIVE
+    $f3->set('username', 'zfrehner');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with templates');
+
+    $f3->set('color', 'purple');
+    $f3->set('radius', 5);
+
+    //define array
+    $f3->set('fruits', array('apple', 'orange', 'banana'));
+
+    $f3->set('bookmarks', array('https://greenriver.edu', 'https://foxnews.com', 'https://www.apple.com'));
+
+    $f3->set('desserts', array('chocolate'=>"Chocolate Mousse", 'vanilla'=>"Vanilla Ice Cream", 'strawberry'=>"Strawberry Stuff"));
+
+
+
+
+
     $view = new Template();
     echo $view->render('views/info.html');
 
